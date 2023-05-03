41ec0dc3b7e9fa2bfea535d6e270800d0069530f
ItaloRochaOliveira <italo.rocha.de.oliveira@gmail.com>
2023-05-03 20:07:03 -0300

    subindo autenticacao do usuario

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,17 +1,17 @@
-import express from "express"
-import { UserBusiness } from "../business/UserBusiness"
-import { UserController } from "../controller/UserController"
-import { UserDatabase } from "../database/UserDatabase"
+import express from "express";
+import { UserBusiness } from "../business/UserBusiness";
+import { UserController } from "../controller/UserController";
+import { UserDatabase } from "../database/UserDatabase";
+import { IdGerator } from "../services/IdGerator";
+import { TokenManage } from "../services/TokenManage";
 
-export const userRouter = express.Router()
+export const userRouter = express.Router();
 
 const userController = new UserController(
-    new UserBusiness(
-        new UserDatabase()
-    )
-)
+  new UserBusiness(new UserDatabase(), new IdGerator(), new TokenManage())
+);
 
-userRouter.get("/", userController.getUsers)
+userRouter.get("/", userController.getUsers);
 
-userRouter.post("/signup", userController.signup)
-userRouter.post("/login", userController.login)
+userRouter.post("/signup", userController.signup);
+userRouter.post("/login", userController.login);
